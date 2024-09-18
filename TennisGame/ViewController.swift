31b113a2c636c6c54cc2f027541e930216253cfd
Alexander <oBelyash@yandex.ru>
2024-09-18 20:09:15 +0300

    Initial Commit

---
+++ TennisGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TennisGame
+//
+//  Created by Александр Андреев on 18.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
